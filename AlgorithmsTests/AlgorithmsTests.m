e418fb6d21d619af49067fb23edfe639a584bc52
Carl Wieland <carlwieland@me.com>
2012-12-05 20:37:09 -0700

    Starting to add test cases for everything

--- AlgorithmsTests/AlgorithmsTests.m
+++ AlgorithmsTests/AlgorithmsTests.m
@@ -7,7 +7,8 @@
 //
 
 #import "AlgorithmsTests.h"
-
+#import "BinarySearchTree.h"
+#import "TreeNode.h"
 @implementation AlgorithmsTests
 
 - (void)setUp
@@ -23,10 +24,49 @@
     
     [super tearDown];
 }
+-(BOOL)checkSide:(TreeNode*)node{
+    if (node!=nil){
+        BOOL left = node.left==nil? true:([node compare:node.left] == NSOrderedDescending);
+        BOOL right = node.right==nil? true:([node compare:node.right] == NSOrderedAscending);
+        BOOL comp = true;
+        if (node.left && node.right) {
+            comp = [node.left compare:node.right]==NSOrderedAscending;
+        }
+        return left && right && comp && [self checkSide:[node left]] && [self checkSide:[node right]];
+    }
+    return true;
+}
+-(void)testBinarySearchTreeBalance{
+    BinarySearchTree* bst = [BinarySearchTree new];
+    for(int i = 0; i<1000; i++){
+        [bst addValue:[NSNumber numberWithInt:arc4random()%5000]];
+    }
+    if(![self checkSide:bst.root])
+        STFail(@"Binary Search Is Not Correctly Balanced!");
 
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in AlgorithmsTests");
+}
+-(void)testBinarySearchRemove{
+    BinarySearchTree* bst = [BinarySearchTree new];
+    int vals[1000];
+    for(int i = 0; i<1000; i++){
+        vals[i]=arc4random()%5000;
+        [bst addValue:[NSNumber numberWithInt:vals[i]]];
+    }
+    for(int i = 0; i<1000; i++){
+        [bst removeValue:[bst.root getValue]];
+    }
+    if(bst.root !=nil){
+        STFail(@"Not Correctly Removing Root!");
+    }
+    for(int i = 0; i<1000; i++){
+        [bst addValue:[NSNumber numberWithInt:vals[i]]];
+    }
+    for(int i = 1000-1; i>=0; i--){
+        [bst removeValue:[NSNumber numberWithInt:vals[i]]];
+    }
+    if(bst.root !=nil){
+        STFail(@"Not Removing Everything!");
+    }
 }
 
 @end
